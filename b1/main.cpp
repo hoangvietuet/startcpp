64324e7a4708e1e825550b9a07deeff550ce9fde
Nguyễn Hoàng Việt <ln0367338@gmail.com>
2025-08-10 15:27:00 +0700

    Update main.cpp

--- b1/main.cpp
+++ b1/main.cpp
@@ -1,9 +1,35 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+typedef long long ll;
+using ll = long long;
+#define ll long long
+#define PI 3.14159
+int main(){
+
+  // vector<int> a[n]; // khai bao array two chieu
+  // string a[n]; // char
+
+  int n; cin >> n;
+  int a[n + 1];
+
+  for (int i = 1; i < n+1; i++) cin >> a[i];
+  int pre[n+1] = {0};
+
+  for (int i = 1; i <= n; i++){
+    pre[i] = pre[i-1] + a[i];
+  }
+
+   int q; cin >> q;
+
+   while(q--){
+    int l, r; cin >> l >> r;
+
+    cout << pre[r] - pre[l-1] << endl;
+   }
 }
+
+
+ //prefex sum [l, r] = pre[r] - pre[l-1];
+
